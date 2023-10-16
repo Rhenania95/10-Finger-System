185ff41db7475952be5c318ff500d2d44907fe4d
alkolb <alkolb@student.ethz.ch>
2023-10-16 17:08:55 +0200

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+Alex war hier.
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.finger_system {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.finger_system to javafx.fxml;
     exports com.example.finger_system;
 }
