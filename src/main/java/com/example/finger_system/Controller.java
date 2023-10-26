4b82c52483b6079944176ffb3a7a0899a8efd4dc
Kosekans <lkruyssee@gmail.com>
2023-10-26 14:36:42 +0200

    Uebungsmodus update

---
+++ src/main/java/com/example/finger_system/Controller.java
@@ -0,0 +1,28 @@
+package com.example.finger_system;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    public void uebungsmodusStart(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- src/main/java/com/example/finger_system/Programmstart.java
+++ src/main/java/com/example/finger_system/Programmstart.java
@@ -3,32 +3,28 @@ package com.example.finger_system;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.Objects;
+
 public class Programmstart extends Application {
-    private InputHandling inputHandling;
-    private Uebungsmodus uebungsmodus;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Programmstart.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-
-            @Override
-            public void handle(KeyEvent event) {
-                if (uebungsmodus.isActive()){
-                    inputHandling.setCurrentCharTyped((event.getCharacter()).charAt(0));
-                    inputHandling.keyPressed();
-                }
-            }
-        });
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("hauptmenu-view.fxml"));
+            Scene hauptmenu = new Scene(root);
+            stage.setTitle("Zehnfinger tipp übungs züg");
+            stage.setScene(hauptmenu);
+            stage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
+
+
 
 
     public static void main(String[] args) {
