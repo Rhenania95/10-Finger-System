b23a75972a32e4a8c28e13842406ff251e0252a5
schueh_11 <simon.joye@students.fhnw.ch>
2023-10-26 15:55:53 +0200

    Überarbeiten übungsauswahl

--- src/main/java/com/example/finger_system/Controller.java
+++ src/main/java/com/example/finger_system/Controller.java
@@ -1,8 +1,12 @@
 package com.example.finger_system;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import java.io.IOException;
 import java.util.Objects;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,12 +18,14 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Controller {
+    public static int modus = 0;
+    public static int level = 0;
     private Stage stage;
     private Scene scene;
     private Parent root;
     @FXML
     public void uebungsmodusStart(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Übungswahl.fxml"));
+        root = FXMLLoader.load(getClass().getResource("auswahlübungen.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
@@ -32,18 +38,186 @@ public class Controller {
         stage.setScene(scene);
         stage.show();
     }
-    public void allese(ActionEvent event) throws IOException {
+    public void alles1(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        modus = 1;
+        level = 1;
     }
-    public void neuee(ActionEvent event) throws IOException {
+    public void alles2(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        modus = 1;
+        level = 2;
     }
+    public void alles3(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 1;
+        level = 3;
+    }
+    public void alles4(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 1;
+        level = 4;
+    }
+    public void alles5(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 1;
+        level = 5;
+    }
+    public void alles6(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 1;
+        level = 6;
+    }    public void alles7(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 1;
+        level = 7;
+    }    public void alles8(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 1;
+        level = 8;
+    }    public void alles9(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 1;
+        level = 9;
+    }
+    public void alles10(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus =1;
+        level = 10;
+    }
+    public void neu1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 2;
+        level = 1;
+    }
+    public void neu2(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 2;
+        level = 2;
+    }
+    public void neu3(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 2;
+        level = 3;
+    }
+    public void neu4(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 2;
+        level = 4;
+    }
+    public void neu5(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 2;
+        level = 5;
+    }
+    public void neu6(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 2;
+        level = 6;
+    }
+    public void neu7(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus = 2;
+        level = 7;
+    }
+    public void neu8(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus=2;
+        level = 8;
+    }
+    public void neu9(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus=2;
+        level = 9;
+    }
+    public void neu10(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("uebungsmodus-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        modus=2;
+        level = 10;
+    }
+
+
+
+
+
 }
