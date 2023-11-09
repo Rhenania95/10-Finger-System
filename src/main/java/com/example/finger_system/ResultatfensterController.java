6cbeec77a0d2e012618300b5fe607030bfcf99ee
Kosekans <lkruyssee@gmail.com>
2023-11-09 14:47:21 +0100

    added Ladels in ResultatfensterController

--- src/main/java/com/example/finger_system/ResultatfensterController.java
+++ src/main/java/com/example/finger_system/ResultatfensterController.java
@@ -3,11 +3,17 @@ package com.example.finger_system;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ResultatfensterController {
+    public Label Zeichenausgabe;
+    public Label Zeichenprominute;
+    public Label Fehlerausgabe;
+    public Label Fehlerquote;
+    public Label Übungsdauer;
     private Stage stage;
     private Scene sceneHauptmenu;
     public void setStage(Stage stage) {
